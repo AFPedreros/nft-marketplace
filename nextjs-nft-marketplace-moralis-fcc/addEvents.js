76e529c2774d87c5305211a4be21650ef4121abd
AFPedreros <felipe.pedreros94@gmail.com>
2022-11-26 11:35:28 -0500

    listen events from ethereum contract

--- nextjs-nft-marketplace-moralis-fcc/addEvents.js
+++ nextjs-nft-marketplace-moralis-fcc/addEvents.js
@@ -1,14 +1,33 @@
-const Moralis = require("moralis/node")
+const Moralis = require("moralis").default
 require("dotenv").config()
 const contractAddresses = require("./constants/networkMapping.json")
 
 let chainId = process.env.chainId || 31337
+const MORALIS_API_KEY = process.env.MORALIS_API_KEY
 const contractAddress = contractAddresses[chainId]["NftMarketplace"][0]
 
-const APPLICATION_ID = process.env.NEXT_PUBLIC_APPLICATION_ID
-const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
+const appId = process.env.NEXT_PUBLIC_APPLICATION_ID
+const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL
 
-async function main() {}
+// 	0x1
+
+async function main() {
+    console.log(`Working with contract address: ${contractAddress}`)
+    await Moralis.start({
+        apiKey: MORALIS_API_KEY,
+        // ...and any other configuration
+    })
+
+    const address = "0xd4e4078ca3495DE5B1d4dB434BEbc5a986197782"
+    const chain = "0x1"
+
+    const response = await Moralis.EvmApi.events.getContractLogs({
+        address,
+        chain,
+    })
+
+    console.log(response.toJSON())
+}
 
 main()
     .then(() => process.exit(0))
